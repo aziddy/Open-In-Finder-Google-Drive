443d4509f6fe66815f1a8ffb723441c7b3469bba
Alex Zidros <alexander.zidros@gmail.com>
2025-07-14 15:45:12 -0400

    added boolean to disable debug logging

--- main.m
+++ main.m
@@ -1,6 +1,12 @@
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
 
+// Debug logging flag
+static const BOOL DEBUG_LOGGING = NO; // Set to YES/NO to enable/disable logging
+
+// Log file path
+static NSString * const LOG_FILE_PATH = @"/tmp/openinfinder.log";
+
 @interface OpenInFinderApp : NSObject <NSApplicationDelegate>
 @end
 
@@ -9,19 +15,23 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
     NSArray *args = [[NSProcessInfo processInfo] arguments];
     
-    // Log the call
-    NSString *logMessage = [NSString stringWithFormat:@"%@: Objective-C app called with args: %@\n", [NSDate date], args];
-    NSString *pwdMessage = [NSString stringWithFormat:@"%@: PWD: %@\n", [NSDate date], [[NSFileManager defaultManager] currentDirectoryPath]];
-    
-    NSString *fullLog = [logMessage stringByAppendingString:pwdMessage];
-    [fullLog writeToFile:@"/tmp/openinfinder.log" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    if (DEBUG_LOGGING) {
+        // Log the call
+        NSString *logMessage = [NSString stringWithFormat:@"%@: Objective-C app called with args: %@\n", [NSDate date], args];
+        NSString *pwdMessage = [NSString stringWithFormat:@"%@: PWD: %@\n", [NSDate date], [[NSFileManager defaultManager] currentDirectoryPath]];
+        
+        NSString *fullLog = [logMessage stringByAppendingString:pwdMessage];
+        [fullLog writeToFile:LOG_FILE_PATH atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    }
     
     if ([args count] > 1) {
         NSString *filePath = [args objectAtIndex:1];
         [self revealInFinder:filePath];
     } else {
-        NSString *usageMessage = [NSString stringWithFormat:@"Usage: %@ <file_path>\n", [args objectAtIndex:0]];
-        [usageMessage writeToFile:@"/tmp/openinfinder.log" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+        if (DEBUG_LOGGING) {
+            NSString *usageMessage = [NSString stringWithFormat:@"Usage: %@ <file_path>\n", [args objectAtIndex:0]];
+            [usageMessage writeToFile:LOG_FILE_PATH atomically:YES encoding:NSUTF8StringEncoding error:nil];
+        }
     }
     
     [NSApp terminate:nil];
@@ -29,8 +39,10 @@
 
 - (BOOL)application:(NSApplication *)sender openFile:(NSString *)filename {
     // This method is called when a file is opened with the app
-    NSString *logMessage = [NSString stringWithFormat:@"%@: Objective-C app openFile called with: %@\n", [NSDate date], filename];
-    [logMessage writeToFile:@"/tmp/openinfinder.log" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    if (DEBUG_LOGGING) {
+        NSString *logMessage = [NSString stringWithFormat:@"%@: Objective-C app openFile called with: %@\n", [NSDate date], filename];
+        [logMessage writeToFile:LOG_FILE_PATH atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    }
     
     [self revealInFinder:filename];
     [NSApp terminate:nil];
@@ -38,8 +50,10 @@
 }
 
 - (void)revealInFinder:(NSString *)filePath {
-    NSString *logMessage = [NSString stringWithFormat:@"%@: Revealing file: %@\n", [NSDate date], filePath];
-    [logMessage writeToFile:@"/tmp/openinfinder.log" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    if (DEBUG_LOGGING) {
+        NSString *logMessage = [NSString stringWithFormat:@"%@: Revealing file: %@\n", [NSDate date], filePath];
+        [logMessage writeToFile:LOG_FILE_PATH atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    }
     
     NSURL *url = [NSURL fileURLWithPath:filePath];
     [[NSWorkspace sharedWorkspace] selectFile:filePath inFileViewerRootedAtPath:[url.URLByDeletingLastPathComponent path]];
